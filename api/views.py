fd24b2cc2af1151f62bc000d20df124e632575eb
Daniil Modanov <modanov.da@gmail.com>
2022-12-18 14:43:05 +0300

    Add methods for users

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
+from django.http import Http404, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from django_filters import rest_framework as filters
 from .serializers import EventsSerializer, TicketsSerializer
 from .models import Events, Tickets
@@ -15,6 +16,8 @@ from django.contrib.auth.models import User
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import generics
 from django.contrib.auth.models import User
+
+
 # from snippets.serializers import UserSerializer
 
 
@@ -26,6 +29,36 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_object(self, pk):
+        try:
+            user = User.objects.get(pk=pk)
+            print(user)
+            return user
+        except User.DoesNotExist:
+            raise Http404
+
+    def list(self):
+        return self.queryset
+
+    def get(self, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    # def patch(self, request, pk):
+    #     user = self.get_object(pk)
+    #
+    #     serializer = UserSerializer(user, data=request.data, partial=True)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return JsonResponse(201, data=serializer.data)
+    #     return JsonResponse(404, data="Wrong parameters. Not found")
 
 
 # Class based view to register user
